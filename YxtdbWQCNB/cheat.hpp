98c4bc38915b6f6fa19c6df0ac515e5496e6cb77
rooticks <rooticks063@gmail.com>
2025-01-13 05:11:06 +0300

    update noflash

--- YxtdbWQCNB/cheat.hpp
+++ YxtdbWQCNB/cheat.hpp
@@ -6,14 +6,21 @@ DWORD64 client = (DWORD64)GetModuleHandle("client.dll");
 
 void NoFlash()
 {
-	
 
-	uintptr_t localplayer = *(uintptr_t*)(client + 0x1868CF8);
 
-	if (localplayer)
-	{
-		*(uintptr_t*)(localplayer + 0x140C) = 0; // m_flFlashDuration
 
-	}
+    while (true)
+    {
+        uintptr_t localplayer = *(uintptr_t*)(client + 0x1869D88);
+        uintptr_t flashDur = *(uintptr_t*)(client + 0x140C);
+        if (localplayer)
+        {
 
+            if (flashDur > 0)
+                *(uintptr_t*)(localplayer + 0x140C) = 0; // m_flFlashDuration
+            if (!noflash) {
+                *(uintptr_t*)(localplayer + 0x140C) = 15; // m_flFlashDuration
+            }
+        }
+    }
 }
